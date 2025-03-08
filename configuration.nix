d0d62d48ce2a6f0cd07f769944a44611977e2cfa
mrsinho <sino3791@gmail.com>
2025-03-08 20:59:32 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -96,9 +96,40 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+	pkgs.neofetch
+	pkgs.tree
+	pkgs.htop
+
+	pkgs.microsoft-edge
+	
+	pkgs.git
+	pkgs.gh
+	pkgs.micro
+	pkgs.vscode
+
+	pkgs.libgcc
+	pkgs.cmake
+	pkgs.gnumake
+	pkgs.gdb
+	pkgs.gcc-arm-embedded
+
+	pkgs.python3Full
+	pkgs.nodePackages.nodejs
+
+	pkgs.openssh
+
+	pkgs.vulkan-tools
+	pkgs.vulkan-loader
+	pkgs.vulkan-helper
+	pkgs.vulkan-headers
+	pkgs.vulkan-extension-layer
+	pkgs.vulkan-validation-layers
+
+	pkgs.glfw
+	
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
