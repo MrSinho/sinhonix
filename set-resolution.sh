52b8b6d8df182b76be347eaf4beaf43dda8cb2f2
Sinho Pascal <68569053+MrSinho@users.noreply.github.com>
2025-03-08 19:38:45 +0100

    Create set-resolution.sh
    When setting up VMs

---
+++ set-resolution.sh
@@ -0,0 +1,4 @@
+gtf 1920 960 120
+xrandr --newmode "1920x960_120.00"  327.96  1920 2072 2288 2656  960 961 964 1029  -HSync +Vsync
+xrandr --addmode Virtual1 "1920x960_120.00"
+xrandr --output Virtual1 --mode "1920x960_120.00"
